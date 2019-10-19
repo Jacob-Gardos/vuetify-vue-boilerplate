88d985e9c5abed3ef1100e2a4da388881e4aa468
12tuesday34 <jacobgardos@gmail.com>
2019-10-19 19:02:22 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app></v-navigation-drawer>
+
+    <v-app-bar app></v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+
+    <v-footer app></v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: () => ({
+    //
+  })
+};
+</script>
